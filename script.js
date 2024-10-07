b9cbcf27600f8c1bb39e7164e61eea7133aa9124
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-10-07 16:07:36 +0530

    Starting off #3

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+const toggleIcon = document.querySelector(".toggle-theme"); 
+
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
+
     if (document.body.classList.contains("dark-mode")) {
-      document.body.style.backgroundColor = "#333";
-      document.body.style.color = "#f0f0f0";
+        document.body.style.backgroundColor = "#1b1f38"; 
+        document.body.style.color = "white"; 
+        toggleIcon.classList.remove('fa-moon'); 
+        toggleIcon.classList.add('fa-sun'); 
     } else {
-      document.body.style.backgroundColor = "#f0f0f0";
-      document.body.style.color = "#333";
+        document.body.style.backgroundColor = "white"; 
+        document.body.style.color = "#1b1f38"; 
+        toggleIcon.classList.remove('fa-sun'); 
+        toggleIcon.classList.add('fa-moon'); 
     }
-  }
+}
 
   function openModal() {
     document.getElementById("myModal").style.display = "flex";
