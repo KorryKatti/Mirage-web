fa63816892ae7e88c74ab486bbab47c51ee9ff37
Ishpreet Singh <112532938+KaranSingh36752@users.noreply.github.com>
2024-10-07 21:47:39 +0530

    Update script.js
    The white theme doesn't make the footer text visible......

--- script.js
+++ script.js
@@ -3,31 +3,35 @@ const toggleIcon = document.querySelector(".toggle-theme");
 function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
 
+    const footer = document.querySelector(".footer");
+
     if (document.body.classList.contains("dark-mode")) {
         document.body.style.backgroundColor = "#1b1f38"; 
         document.body.style.color = "white"; 
+        footer.style.color = "white"; 
         toggleIcon.classList.remove('fa-moon'); 
         toggleIcon.classList.add('fa-sun'); 
     } else {
         document.body.style.backgroundColor = "white"; 
         document.body.style.color = "#1b1f38"; 
+        footer.style.color = "#1b1f38";
         toggleIcon.classList.remove('fa-sun'); 
         toggleIcon.classList.add('fa-moon'); 
     }
 }
 
-  function openModal() {
+function openModal() {
     document.getElementById("myModal").style.display = "flex";
-  }
+}
 
-  function closeModal() {
+function closeModal() {
     document.getElementById("myModal").style.display = "none";
-  }
+}
 
-  // Close the modal when clicking outside of the content
-  window.onclick = function (event) {
+
+window.onclick = function (event) {
     const modal = document.getElementById("myModal");
     if (event.target == modal) {
-      closeModal();
+        closeModal();
     }
-  };
+};
