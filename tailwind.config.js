ebf79056b7e309beeb27baa08835e667ce7172cc
karan <karansingh36752@gmail.com>
2024-10-12 15:31:34 +0530

    ui update

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'], 
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
