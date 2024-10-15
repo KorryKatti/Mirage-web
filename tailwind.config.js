f5c55df0db11db62acc32710c1fe20d7dc3f02d7
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-10-15 12:43:06 +0530

    style and layout formatted properly

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        'custom-blue': 'rgb(27, 31, 56)',
+        'custom-pink': 'rgb(255, 71, 126)',
+      },
       fontFamily: {
+        sora: ['Sora', 'sans-serif'], 
         manrope: ['Manrope', 'sans-serif'], 
         inter: ['Inter', 'sans-serif'],
       },
