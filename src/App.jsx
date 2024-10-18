161e26a4805a206ee103039221112163c3ae77ae
mahenoorsalat <salatmahenoor7.8.6@gmail.com>
2024-10-18 12:08:21 +0530

    UI updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Body from './components/Body';
+import {useState} from 'react';
 function App() {
+  const [toggleTheme, setToggleTheme] = useState(false);
+
+  const handleToggle = () => {
+    setToggleTheme((prev) => !prev);
+  };
+
   return (
-      <Body/>
+    <Body toggleTheme={toggleTheme} handleToggle={handleToggle} />
   );
-}
+};
+
 
 
 export default App;
