ea9364957659d3db4515c64a2df99d56f19fe5c2
KorryKatti <143781663+KorryKatti@users.noreply.github.com>
2024-10-18 14:46:59 +0530

    Revert "UI updated"

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Body from './components/Body';
-import {useState} from 'react';
 function App() {
-  const [toggleTheme, setToggleTheme] = useState(false);
-
-  const handleToggle = () => {
-    setToggleTheme((prev) => !prev);
-  };
-
   return (
-    <Body toggleTheme={toggleTheme} handleToggle={handleToggle} />
+      <Body/>
   );
-};
-
+}
 
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ module.exports = {
       fontFamily: {
         manrope: ['Manrope', 'sans-serif'], 
         inter: ['Inter', 'sans-serif'],
-        sora: ['Sora', 'sans-serif'], // Add Sora font
-      },
-      colors: {
-        'custom-blue': '#1b1f38', // Custom blue color
-        'custom-gray': '#f8f9fa', // Add another color if needed
       },
     },
   },
